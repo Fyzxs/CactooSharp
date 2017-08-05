2e18da4e46b158190075cb8f9c331d3e8fc1a58c
Fyzxs <Fyzxs@users.noreply.github.com>
2017-08-04 19:31:38 -0700

    Scalar and NoNulls are done

---
+++ CactooSharp.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CactooSharp.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CactooSharp.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7bb60e12-43d3-416b-be9c-ccfb3b61d173")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CactooSharp.Tests/UnitTest1.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+
+namespace CactooSharp.Tests
+{
+
+    [TestClass]
+    public class ScalarTests
+    {
+        [TestMethod]
+        public void ShouldFailForNullArgument()
+        {
+            ((Action)(() => new NoNulls<object>(scalar: null).Value()))
+                .ShouldThrow<ArgumentException>()
+                .WithMessage("NULL instead of a valid scalar");
+        }
+
+        [TestMethod]
+        public void ShouldFailForNullValue()
+        {
+            Mock<Scalar<object>> mock = new Mock<Scalar<object>>();
+            ((Action)(() => new NoNulls<object>(scalar: mock.Object).Value()))
+                .ShouldThrow<ArgumentException>()
+                .WithMessage("NULL instead of a valid value");
+        }
+
+        [TestMethod]
+        public void ShouldBeOkForNoNulls()
+        {
+            object obj = new object();
+            Mock<Scalar<object>> mock = new Mock<Scalar<object>>();
+            mock.Setup(x => x.Value()).Returns(obj);
+            new NoNulls<object>(scalar: mock.Object).Value().Should().BeSameAs(obj);
+        }
+    }
+
+    public class NoNulls<T> : Scalar<T> where T : class
+    {
+        private readonly Scalar<T> _origin;
+
+        public NoNulls(Scalar<T> scalar) => _origin = scalar;
+
+        public T Value()
+        {
+            if (_origin == null) throw new ArgumentException("NULL instead of a valid scalar");
+
+            T value = _origin.Value();
+            if (value == null) throw new ArgumentException("NULL instead of a valid value");
+
+            return value;
+        }
+    }
+
+    // ReSharper disable once InconsistentNaming
+    public interface Scalar<out T> where T : class
+    {
+        T Value();
+    }
+}
