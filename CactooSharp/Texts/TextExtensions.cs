44b7e601181d6bc0ea6f90aac3a12f0734037933
Fyzxs <Fyzxs@users.noreply.github.com>
2017-08-04 22:45:20 -0700

    Added TextExtensions

---
+++ CactooSharp/Texts/TextExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace CactooSharp.Texts
+{
+    public static class TextExtensions
+    {
+        public static Text ToLower(this Text text) => new ToLower(text);
+        public static Text ToLower(this Text text, CultureInfo cultureInfo) => new ToLower(text, cultureInfo);
+        public static Text ToUpper(this Text text) => new ToUpper(text);
+        public static Text ToUpper(this Text text, CultureInfo cultureInfo) => new ToUpper(text, cultureInfo);
+    }
+}
