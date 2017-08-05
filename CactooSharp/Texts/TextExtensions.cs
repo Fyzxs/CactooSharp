543770df1bfea5176ce81fe1b6684456903b90e7
Fyzxs <Fyzxs@users.noreply.github.com>
2017-08-04 22:53:45 -0700

    Added some extension methods

--- CactooSharp/Texts/TextExtensions.cs
+++ CactooSharp/Texts/TextExtensions.cs
@@ -4,9 +4,15 @@ namespace CactooSharp.Texts
 {
     public static class TextExtensions
     {
+        public static Text ToText(this string value) => new TextOf(value);
+        public static Text ToLower(this string value) => value.ToText().ToLower();
+
+
         public static Text ToLower(this Text text) => new ToLower(text);
         public static Text ToLower(this Text text, CultureInfo cultureInfo) => new ToLower(text, cultureInfo);
+
         public static Text ToUpper(this Text text) => new ToUpper(text);
         public static Text ToUpper(this Text text, CultureInfo cultureInfo) => new ToUpper(text, cultureInfo);
+
     }
 }
