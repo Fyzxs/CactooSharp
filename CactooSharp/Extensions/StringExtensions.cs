35eb8820a9e63813917b76e3d9ba85bb0a5e1ca5
Fyzxs <Fyzxs@users.noreply.github.com>
2017-08-05 06:07:30 -0700

    Removing string constructors, split extensions into Text and String

--- CactooSharp/Extensions/StringExtensions.cs
+++ CactooSharp/Extensions/StringExtensions.cs
@@ -1,5 +1,9 @@
-namespace CactooSharp.Texts {
-    public static class StringExtensions {
+using CactooSharp.Texts;
+
+namespace CactooSharp.Extensions
+{
+    public static class StringExtensions
+    {
         public static Text ToText(this string value) => new TextOf(value);
         public static Text ToLower(this string value) => value.ToText().ToLower();
         public static Text ToUpper(this string value) => value.ToText().ToUpper();
